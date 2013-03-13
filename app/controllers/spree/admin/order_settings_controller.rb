f1629efbf4d168b1bba53e685b69257ecba18780
cis <cis@CISM257.(none)>
2013-03-13 19:02:55 +0530

    show values in order setting

--- app/controllers/spree/admin/order_settings_controller.rb
+++ app/controllers/spree/admin/order_settings_controller.rb
@@ -13,11 +13,14 @@ class Spree::Admin::OrderSettingsController < Spree::Admin::BaseController
       @first = OrderSetting.first
       @order_setting =@first.update_attributes(params[:order_setting])
     end
-      redirect_to edit_admin_general_settings_path
+      redirect_to edit_admin_order_setting_path(OrderSetting.first.id)
   rescue 
       render action: "new"  
   end
  end  
 
+  def edit
+    @order_setting = OrderSetting.find(params[:id])
+  end
 
 end
